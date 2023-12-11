c5bf5a1f066674ebf5dbb84fe76f540258852723
alexforrester <aforresterwork@gmail.com>
2023-12-11 18:06:59 +0000

    Adds sample input

--- src/Day07_part1.kt
+++ src/Day07_part1.kt
@@ -1,27 +1,76 @@
-sealed interface CardHands
+sealed interface CardType
 
-@JvmInline
-value class FiveOfAKind(val hand: String) : CardHands
+data class FiveOfAKind(val hand: String, val value: Int) : CardType
+data class FourOfAKind(val hand: String, val value: Int) : CardType
+data class FullHouse(val hand: String, val value: Int) : CardType
+data class ThreeOfAKind(val hand: String, val value: Int) : CardType
+data class TwoPair(val hand: String, val value: Int) : CardType
+data class OnePair(val hand: String, val value: Int) : CardType
+data class HighestCard(val hand: String, val value: Int) : CardType
 
-@JvmInline
-value class FourOfAKind(val hand: String) : CardHands
-
-@JvmInline
-value class FullHouse(val hand: String) : CardHands
-
-@JvmInline
-value class ThreeOfAKind(val hand: String) : CardHands
-
-@JvmInline
-value class TwoPair(val hand: String) : CardHands
-
-@JvmInline
-value class OnePair(val hand: String) : CardHands
-
-@JvmInline
-value class HighestCard(val hand: String) : CardHands
+// 32T3K 765
+// T55J5 684
+// KK677 28
+// KTJJT 220
+// QQQJA 483
 
 fun main() {
+
+    // Create linkedhashmap of hands to value
+    // Add all rows into data classes based on what kind of hand they are
+
+    // Filter down and use when statement
+    // Are Filter down from 5 of a kind - count 5 the same
+    // Are Filter down from 4 of a kind - count 4 the same
+    // Has 3 of a kind with  two of a kind
+    // Has 3 of a kind
+    // Has 2 pairs
+    // Has 1 pair
+    // Highest card
+
+    // Once determined then add to correct list
+
+    // Loop round the lists
+
+
+
+
+
+
+
+    //
+
+
+    // End up with seven lists
+
+    // For each list type get the size of the list
+
+    // Loop round the list
+
+
+
+    // Add each
+
+    // After
+
+    // Have a map of hands orders to list of card hands data classes
+    // Loop through
+
+
+
+
+    // Add each to a list
+
+    // Put all values into a map
+
+
+
+
+
+    // Determine kind and return data class
+    // Associate by rank
+    // descending
+
 
     // Determine 5 of a kind
     // Determine 4 of a kind
@@ -30,6 +79,9 @@ fun main() {
     // Determine 2 pair
     // Determine 1 pair
     // Determine highest card
+
+
+    // Compare
 
 
 }
